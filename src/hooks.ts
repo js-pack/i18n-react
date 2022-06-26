f28666c5bcda690f432a3bb5643901d842706a1e
engin ustun <enginustun@outlook.com>
2022-06-26 16:04:57 +0300

    feat: Add locale getter to hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,9 +3,12 @@ import { useContext } from 'react';
 import I18nContext from './i18nContext';
 
 export const useTranslation = () => {
-  useContext(I18nContext);
+  const { locale } = useContext(I18nContext);
 
   return {
+    get locale() {
+      return locale;
+    },
     get t() {
       return t;
     },
